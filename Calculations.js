ec66e96a1a087897c554a04a9190dd0bbf39304a
zahidhai76 <45167319+zahidhai76@users.noreply.github.com>
2022-06-25 22:31:36 +0100

    Add files via upload

--- Calculations.js
+++ Calculations.js
@@ -1,35 +1,93 @@
-function taxable(){
-    var taxFreeAmount, taxableAmount, tax;
-    if(document.getElementById("tax code").value == "1257L"){
-        taxFreeAmount = 1047.50;
-    } 
-    else if (document.getElementById("tax code").value == "1170L"){
-       taxFreeAmount = 975;
+function unhideInputSingle() {
+    for (var i = 0; i < 1; i++){
+    if(document.getElementById("period").value == "Annually") {
+        document.getElementsByClassName("gross-monthly")[i].style.display = "none";
+        document.getElementsByClassName("gross-annually")[i].style.display = "block";
     }
-    if(document.getElementById("gross-monthly").value >= 1047.50)
+    else if (document.getElementById("period").value == "Monthly") {
+        document.getElementsByClassName("gross-annually")[i].style.display = "none"
+        document.getElementsByClassName("gross-monthly")[i].style.display = "block"
+    }
+    }
+}
+
+function unhideInputOther() {
+    for (var i = 0; i < 1; i++){
+    if(document.getElementById("period").value == "Annually") {
+        document.getElementsByClassName("gross-monthly")[i].style.display = "block";
+        document.getElementById("gross-monthly").disabled = true;
+        document.getElementById("gross-annually").disabled = false;
+        document.getElementById("calculateOtherButton").innerHTML = "Calculate Gross Monthly";
+    }
+    else if (document.getElementById("period").value == "Monthly") {
+        document.getElementsByClassName("gross-annually")[i].style.display = "block"
+        document.getElementById("gross-annually").disabled = true;
+        document.getElementById("gross-monthly").disabled = false;
+        document.getElementById("calculateOtherButton").innerHTML = "Calculate Gross Annually";
+    }
+    }
+}
+
+function grossMonthly() {
+    var grossMonth;
+    if(document.getElementById("period").value == "Annually"){
+        grossMonth = document.getElementById("gross-annually").value / 12;
+        document.getElementById("gross-monthly").value = grossMonth.toFixed(2);
+    } 
+    else if (document.getElementById("period").value == "Monthly"){
+        grossMonth = document.getElementById("gross-monthly").value;
+        let grossAnnually = grossMonth * 12;
+        document.getElementById("gross-annually").value = grossAnnually.toFixed(2);
+    }
+}
+
+function taxable(){
+    var taxFreeAmount, taxableAmountLower, taxableAmountHigher, taxLower, taxHigher;
+    let gross = document.getElementById("gross-monthly").value - PensionContribution();
+    if(document.getElementById("tax-code").value == "1257L"){
+        taxFreeAmount = 1048.59;
+    } 
+    else if (document.getElementById("tax-code").value == "1170L"){
+        taxFreeAmount = 975;
+    }
+    if(gross >= 1048.59)
     {
-        taxableAmount = parseInt(document.getElementById("gross-monthly").value) - taxFreeAmount,2;
+        taxableAmountLower = gross - taxFreeAmount;
+        if(gross >= 4189)
+        {
+            taxableAmountHigher = gross - 4189;
+        }
+        else {
+            taxableAmountHigher = 0;
+        }
     }
     else {
-        taxableAmount = 0;
+        taxableAmountLower = 0;
+        taxableAmountHigher = 0;
     }
-    document.getElementById("taxable").innerHTML = "£" + taxableAmount.toFixed(2);
-    tax = 0.2 * taxableAmount;
-    document.getElementById("tax").innerHTML = "£" + tax.toFixed(2);
-    return tax;
+    document.getElementById("tax20").innerHTML = "£" + taxableAmountLower.toFixed(2);
+    document.getElementById("tax40").innerHTML = "£" + taxableAmountHigher.toFixed(2);
+    taxLower = 0.2 * taxableAmountLower;
+    taxHigher = 0.4 * taxableAmountHigher;
+    document.getElementById("taxed20").innerHTML = "£" + taxLower.toFixed(2);
+    document.getElementById("taxed40").innerHTML = "£" + taxHigher.toFixed(2);
+    let taxTotal = taxLower + taxHigher;
+    document.getElementById("tax").innerHTML = "£" + taxTotal.toFixed(2);
+    return taxTotal;
 }
 
 function NIApplied(){
-    var NIFreeAmount, NIAmount, NI;
-    if(document.getElementById("gross-monthly").value >= 823.01){
-        NIFreeAmount = 823.01
-        NIAmount = parseInt(document.getElementById("gross-monthly").value) - NIFreeAmount,2;
+    var NIFreeAmountLower, NIAmount, NI;
+    let gross = document.getElementById("gross-monthly").value - PensionContribution();
+    if(gross >= 823.01){
+        NIFreeAmountLower = 823.01
+        NIAmount = gross - NIFreeAmountLower;
     } 
     else {
-        NIFreeAmount = 0;
+        NIFreeAmountLower = 0;
         NIAmount = 0;
     }
-    document.getElementById("NI applied on").innerHTML = "£" + NIAmount.toFixed(2);
+    document.getElementById("NI-applied-on").innerHTML = "£" + NIAmount.toFixed(2);
     NI = 0.1325 * NIAmount;
     document.getElementById("NI").innerHTML = "£" + NI.toFixed(2);
     return NI;
@@ -44,19 +102,27 @@ function SLApplied(){
         SLFreeAmount = 2274;
     }
     if(document.getElementById("gross-monthly").value >= SLFreeAmount){
-        SLAmount = parseInt(document.getElementById("gross-monthly").value) - SLFreeAmount,2;
+        SLAmount = parseInt(document.getElementById("gross-monthly").value) - SLFreeAmount - PensionContribution();
     } 
     else {
         SLFreeAmount = 0;
         SLAmount = 0;
     }
-    document.getElementById("SL applied on").innerHTML = "£" + SLAmount.toFixed(2);
+    document.getElementById("SL-applied-on").innerHTML = "£" + SLAmount.toFixed(2);
     SL = 0.09 * SLAmount;
     document.getElementById("SL").innerHTML = "£" + SL.toFixed(2);
     return SL;
 }
 
+function PensionContribution(){
+    let PensionPercentage = document.getElementById("pension-contribution").value / 100;
+    let grossMonthly = document.getElementById("gross-monthly").value;
+    const pension = PensionPercentage * grossMonthly;
+    document.getElementById("pension").innerHTML = "£" + pension.toFixed(2);
+    return pension;
+}
+
 function TakeHome(){
-    let TakeHomeAmount = document.getElementById("gross-monthly").value - taxable() - NIApplied() - SLApplied();
+    let TakeHomeAmount = document.getElementById("gross-monthly").value - taxable() - NIApplied() - SLApplied() - PensionContribution();
     document.getElementById("TakeHome").innerHTML = "£" + TakeHomeAmount.toFixed(2);
 }
